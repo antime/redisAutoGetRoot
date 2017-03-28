82608d2d2f12ea2a7fa28e69628c961b8c5f9889
myDreamShadow <345919932@qq.com>
2017-03-28 13:46:15 +0800

    Create ShoDanAPI.py

---
+++ ShoDanAPI.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#_*_ encoding:utf-8 _*_
+
+import shodan
+
+class ShoDanSearch(object):
+
+    SHODAN_API_KEY = 'ILWLAUb4NElNxnJAi5SrEH8rCnVMsjLG'
+
+    def __init__(self, keywords):
+        self.keywords = keywords
+        self.api = shodan.Shodan(self.SHODAN_API_KEY)
+        self.ipList = []
+
+    def getKeyString(self):
+        print self.SHODAN_API_KEY
+
+    def getSearchInfo(self):
+        try:
+            result = self.api.search(self.keywords)
+            print 'Results found: %s' % result['total']
+            for res in result['matches']:
+                # print res['ip_str']
+                self.ipList.append(res['ip_str'])
+            return self.ipList
+        except shodan.APIError,e:
+            print 'Error: %s' % e
