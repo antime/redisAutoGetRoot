f75de6c294b35b4b0b6fa54f62afd94d0ec9852b
myDreamShadow <345919932@qq.com>
2017-03-28 17:45:39 +0800

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#_*_ encoding: utf-8 _*_
+
+#国际域名
+class CountryDomain:
+
+    domain = [
+        'ad', 'ae', 'af', 'ag', 'ai', 'al', 'am', 'ao', 'ar', 'at', 'au', 'az', 'bb', 'bd', 'be', 'bf', 'bg',
+        'bh', 'bi', 'bj', 'bl', 'bm', 'bn', 'bo', 'br', 'bs', 'bw', 'by', 'bz', 'ca', 'cf', 'cg', 'ch', 'ck',
+        'cl', 'cm', 'cn', 'co', 'cr', 'cs', 'cu', 'cy', 'cz', 'de', 'dj', 'dk', 'do', 'dz', 'ec', 'ee', 'eg',
+        'es', 'et', 'fi', 'fj', 'fr', 'ga', 'gb', 'gd', 'ge', 'gf', 'gh', 'gi', 'gm', 'gn', 'gr', 'gt', 'gu',
+        'gy', 'hk', 'hn', 'ht', 'id', 'ie', 'il', 'in', 'iq', 'ir', 'is', 'it', 'jm', 'jo', 'jp', 'kg', 'kh',
+        'kp', 'kr', 'kt', 'kw', 'kz', 'la', 'lb', 'lc', 'li', 'lk', 'lr', 'ls', 'lt', 'lu', 'lv', 'ly', 'ma',
+        'mc', 'md', 'mg', 'ml', 'mm', 'mn', 'mo', 'ms', 'mt', 'mu', 'mv', 'mw', 'mx', 'my', 'mz', 'na', 'ne',
+        'ng', 'ni', 'nl', 'no', 'np', 'nr', 'nz', 'om', 'pa', 'pe', 'pf', 'pg', 'ph', 'pk', 'pl', 'pr', 'pt',
+        'py', 'qa', 'ro', 'ru', 'sa', 'sb', 'sc', 'sd', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sr', 'st',
+        'sv', 'sy', 'sz', 'td', 'tg', 'th', 'tj', 'tm', 'tn', 'to', 'tr', 'tt', 'tw', 'tz', 'ua', 'ug', 'us',
+        'uy', 'uz', 'vc', 've', 'vn', 'ye', 'yu', 'za', 'zm', 'zr', 'zw'
+    ]
+
+#zoomeye 登陆账号密码
+class zoomeyeLoginInfo:
+
+    username = ''
+    password = ''
