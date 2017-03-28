1153871d47dc1b03009c59ffcffe1ce368b7f58a
myDreamShadow <345919932@qq.com>
2017-03-28 13:45:36 +0800

    Create zoomeyeAPI.py

---
+++ zoomeyeAPI.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#_*_ encoding: utf-8 _*_
+
+import zoomeye
+
+class ZoomeyeAPI(object):
+
+    username = ''
+    password = ''
+
+    def __init__(self, keyword):
+        self.__zoom = zoomeye.ZoomEye(self.username, self.password)
+        self.__login = self.__zoom.login()
+        self.keyword = keyword
+
+    def getLogin(self):
+        return self.__login
+
+    def getSearchInfo(self, content, page=0):
+        data = self.__zoom.dork_search(dork=content, page=page, resource='host')
+        return data
+
+    def getHostInfo(self):
+        ips = []
+        for p in xrange(50):
+            search_data = self.getSearchInfo(content=self.keyword, page=p)
+            for sd in search_data: ips.append(sd['ip'])
+        return list(set(ips))
